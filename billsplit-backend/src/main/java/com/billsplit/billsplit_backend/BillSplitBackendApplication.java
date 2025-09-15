81525350411322224541cb1a92a437bc32e4eeb9
Rishi Nivedan <rishinvdn03@gmail.com>
2025-09-15 21:55:07 +0530

    Initial commit: Spring Boot backend setup

---
+++ billsplit-backend/src/main/java/com/billsplit/billsplit_backend/BillSplitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.billsplit.billsplit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSplitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillSplitBackendApplication.class, args);
+	}
+
+}
